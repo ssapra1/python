cbd60fc53c81057d4803576b96760966f5880f8b
ssapra1 <sapra.saksham1@gmail.com>
2024-12-10 21:28:25 +0530

    commited changes

---
+++ productionpyhton/SalesHistoricalData.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import time
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+def main():
+    st.title("Add Sales historical data to database")
+    with st.form("sales_form1"):
+
+     uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+     submitted1 = st.form_submit_button('upload')
+     st.write(submitted1)
+    if submitted1:
+       if uploaded_file is not None:
+        with st.spinner('Reading from CSV file..'):
+               time.sleep(20)
+        data = pd.read_csv(uploaded_file)
+
+        st.write("Here is the uploaded file's data:")
+        st.dataframe(data)
+
+        st.line_chart(data)
+        st.write("Material-style data table:")
+
+
+        import sqlite3
+
+        # Assuming `data` is the DataFrame from the uploaded file
+        column_headers = ["ID", "Date", "Brand", "Weight (kg)", "Category", "PHR", "Bundle Count",
+               "Pipe Count"]
+        # Ensure the uploaded file contains required columns
+        if all(col in data.columns for col in column_headers):
+            # Create a connection to the SQLite database (or create the database if it doesn't exist)
+            conn = sqlite3.connect('production_form.db')
+            cursor = conn.cursor()
+
+            # Create table with the specified columns if it does not exist
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS SalesFormData (
+                     ID Text,
+                                date TEXT,
+                                brand TEXT,
+                                weight REAL,
+                                category TEXT,
+                                phr TEXT,
+                                bundle_count INTEGER,
+                                pipe_count INTEGER
+                )
+            ''')
+
+            # Insert all data into the database
+            with st.spinner('Inserting data into database...'):
+                time.sleep(2)  # Optional delay to simulate loading effect
+                # Insert all data into the database
+                for _, row in data[column_headers].iterrows():
+                    cursor.execute('''
+                             INSERT INTO SalesFormData (date, brand, weight, category, phr, bundle_count, pipe_count)
+                            VALUES (?, ?, ?, ?, ?, ?, ?)
+                          ''', tuple(row))
+            # Commit changes and close the connection
+            conn.commit()
+            conn.close()
+
+            st.write("Data has been inserted into the database successfully.")
+        else:
+            st.write("The uploaded file does not contain all required columns.")
+if __name__ == "__main__":
+    main()

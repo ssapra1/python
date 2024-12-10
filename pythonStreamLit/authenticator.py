ac7f730f13d7787ae71ccf80e560e167798e484a
ssapra1 <sapra.saksham1@gmail.com>
2024-12-11 00:01:33 +0530

    commited changes

--- pythonStreamLit/authenticator.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-# Authentication components
-def authenticate(username, password):
-    # Replace 'user' and 'pass' with your actual username and password
-    return username == "user" and password == "pass"
-
-
